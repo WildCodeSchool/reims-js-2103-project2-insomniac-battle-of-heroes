2615a3ab6fff8706d771f57da4ed75cd4e5dc1aa
HuguesBriqueler <hugues.briqueler@gmail.com>
2021-04-16 09:35:51 +0200

    Renamed module draw.js with english variable names

--- src/components/draw.js
+++ src/components/draw.js
@@ -1,8 +1,6 @@
-const tirage = (taille, maxValue = 730) => {
-  const arr = new Array(taille).fill(1);
+const draw = (howManyHeroes, maxValue = 730) => {
+  const arr = new Array(howManyHeroes).fill(1);
   const resultat = arr.map((randomValue) => randomValue * Math
     .floor(Math.random() * maxValue) + 1);
   return resultat;
 };
-
-export default tirage;
