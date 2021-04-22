0f08f1a94ce4aac4e47e2a22e64be56a7da0ae33
Timothee <augustin.timothee@gmail.com>
2021-04-22 15:49:38 +0200

    change class of a card on click

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,20 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import Game from './components/Game';
-import drawCards from './components/drawCards';
 import './App.css';
 
 function App() {
-  const [heroList, setHeroList] = useState([]);
-  const [villainList, setVillainList] = useState([]);
-
-  useEffect(() => {
-    drawCards(5, setHeroList);
-    drawCards(5, setVillainList);
-  }, []);
-
   return (
     <>
       <div className="App">
-        <body>
-          <div className="heroAvatar">
-            <img src="https://nsa40.casimages.com/img/2021/04/15/210415123011254599.png" id="titleLogo" alt="title-logo" />
-          </div>
-          <div className="navBar">
-            <button type="button" className="buttonOnMenu1">Menu</button>
-            <button type="button" className="buttonOnMenu2">Play</button>
-            <button type="button" className="buttonOnMenu3">Collection</button>
-          </div>
-          <Game
-            heroList={heroList}
-            setHeroList={setHeroList}
-            villainList={villainList}
-            setVillainList={setVillainList}
-          />
-        </body>
+        <div className="heroAvatar">
+          <img src="https://nsa40.casimages.com/img/2021/04/15/210415123011254599.png" id="titleLogo" alt="title-logo" />
+        </div>
+        <div className="navBar">
+          <button type="button" className="buttonOnMenu1">Menu</button>
+          <button type="button" className="buttonOnMenu2">Play</button>
+          <button type="button" className="buttonOnMenu3">Collection</button>
+        </div>
+        <Game />
       </div>
     </>
   );
--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,35 +1,48 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React, { useState, useEffect } from 'react';
 import Card from './Card';
 import drawCards from './drawCards';
 import '../App.css';
 
-function Game({
-  heroList, setHeroList, villainList, setVillainList,
-}) {
+function Game() {
+  const [heroList, setHeroList] = useState([]);
+  const [villainList, setVillainList] = useState([]);
+  const [fighterId, setFighterId] = useState('');
+
+  useEffect(() => {
+    drawCards(5, setHeroList);
+    drawCards(5, setVillainList);
+  }, []);
+
   return (
     <div className="board">
       <div className="heroSide cards">
         <button type="button" onClick={() => drawCards(1, setHeroList)} className="drawButton">
           Draw hero
         </button>
-        {heroList.map((hero) => (<Card className="heroHand" avatar={hero} />))}
+        {heroList.map((hero) => (
+          <Card
+            key={hero.id}
+            avatar={hero}
+            fighterId={fighterId}
+            setFighterId={setFighterId}
+          />
+        ))}
       </div>
       <div className="villainSide cards">
         <button type="button" onClick={() => drawCards(1, setVillainList)} className="drawButton">
           Draw villain
         </button>
-        {villainList.map((villain) => (<Card className="villainHand" avatar={villain} />))}
+        {villainList.map((villain) => (
+          <Card
+            key={villain.id}
+            avatar={villain}
+            fighterId={fighterId}
+            setFighterId={setFighterId}
+          />
+        ))}
       </div>
     </div>
   );
 }
-
-Game.propTypes = {
-  heroList: PropTypes.arrayOf(PropTypes.object).isRequired,
-  setHeroList: PropTypes.func.isRequired,
-  villainList: PropTypes.arrayOf(PropTypes.object).isRequired,
-  setVillainList: PropTypes.func.isRequired,
-};
 
 export default Game;
