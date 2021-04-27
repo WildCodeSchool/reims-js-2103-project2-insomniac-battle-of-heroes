6fceae6eabc2928fb915297d899211d22d3e5b66
Timothee <augustin.timothee@gmail.com>
2021-04-27 10:23:25 +0200

    modified draw to excludes null in 60 cards

--- src/components/draw.js
+++ src/components/draw.js
@@ -1,8 +1,11 @@
-const draw = (howManyHeroes, maxValue = 730) => {
-  const arr = new Array(howManyHeroes).fill(1);
-  const result = arr.map((randomValue) => randomValue * Math
+const draw = (howManyHeroes, maxValue = 60) => {
+  const array = new Array(maxValue).fill(1);
+  const excludeCards = [9, 16, 19, 21, 22, 27, 33, 46, 47, 50, 51, 54, 55, 59];
+  const randomArray = array.map((randomValue) => randomValue * Math
     .floor(Math.random() * maxValue) + 1);
-  return result;
+  const filteredArray = randomArray.filter((card) => !excludeCards.includes(card));
+  const slicedArray = filteredArray.slice((filteredArray.length - howManyHeroes));
+  return slicedArray;
 };
 
 export default draw;
