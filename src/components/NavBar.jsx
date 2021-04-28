8a1ea9c92ef94cb969fe579dd2c4e83aa31d524f
Jean-Marc Epis <jeanmarc.epis@gmail.com>
2021-04-28 10:04:51 +0200

    Deleted useless react fragments

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -11,27 +11,25 @@ import Home from './Home';
 function NavBar() {
   return (
     <Router>
-      <>
-        <div className="App">
-          <div className="logoContent">
-            <img src="./images/logo-iboh-flat-400.png" id="titleLogo" alt="title-logo" />
-            <span id="logoBack" />
-          </div>
-          <div className="navBar">
-            <NavLink exact to="/" type="button" activeClassName="active" className="buttonLink1">Menu</NavLink>
-            <NavLink to="/game" type="button" activeClassName="active" className="buttonLink2">Play</NavLink>
-            <NavLink to="/collection" type="button" activeClassName="active" className="buttonLink3">Collection</NavLink>
-          </div>
+      <div className="App">
+        <div className="logoContent">
+          <img src="./images/logo-iboh-flat-400.png" id="titleLogo" alt="title-logo" />
+          <span id="logoBack" />
         </div>
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route exact path="/game">
-            <Game />
-          </Route>
-        </Switch>
-      </>
+        <div className="navBar">
+          <NavLink exact to="/" type="button" activeClassName="active" className="buttonLink1">Menu</NavLink>
+          <NavLink to="/game" type="button" activeClassName="active" className="buttonLink2">Play</NavLink>
+          <NavLink to="/collection" type="button" activeClassName="active" className="buttonLink3">Collection</NavLink>
+        </div>
+      </div>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/game">
+          <Game />
+        </Route>
+      </Switch>
     </Router>
   );
 }
