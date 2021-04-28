fc166932e5cb3307206ef07b135ab6b0779aa2e6
Jean-Marc Epis <jeanmarc.epis@gmail.com>
2021-04-28 09:40:26 +0200

    Deleted comments in the code

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,5 @@
 import React from 'react';
 import NavBar from './components/NavBar';
-// import {
-//   BrowserRouter as Router,
-//   Switch,
-//   Route,
-//   NavLink,
-// } from 'react-router-dom';
-// import Game from './components/Game';
-// import Home from './components/Home';
-
 import './App.css';
 
 function App() {
