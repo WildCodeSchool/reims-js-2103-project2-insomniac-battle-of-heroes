ffb16be8fef436a0286da09ea577a1cb2240e382
Jean-Marc Epis <jeanmarc.epis@gmail.com>
2021-04-28 10:45:39 +0200

    Fixed all problems.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,33 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
 import NavBar from './components/NavBar';
 import './App.css';
+import Game from './components/Game';
+import Home from './components/Home';
 
 function App() {
   return (
-    <NavBar />
+    <div className="App">
+      <div className="logoContent">
+        <img src="./images/logo-iboh-flat-400.png" id="titleLogo" alt="title-logo" />
+        <span id="logoBack" />
+      </div>
+      <Router>
+        <NavBar />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/game">
+            <Game />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
   );
 }
 
