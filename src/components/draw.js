e78e9d696126c5ee90eae74b56575744434b9a3a
Timothee <augustin.timothee@gmail.com>
2021-04-28 10:04:49 +0200

    requiered changes

--- src/components/draw.js
+++ src/components/draw.js
@@ -1,12 +1,22 @@
 const draw = (howManyHeroes, maxValue = 100) => {
-  const array = new Array(maxValue).fill(1);
-  const excludeCards = [9, 16, 19, 21, 22, 27, 33, 46, 47, 50, 51, 54, 55, 59, 62, 64, 65, 67, 74,
-    77, 85, 86, 89, 90, 91, 94];
-  const randomArray = array.map((randomValue) => randomValue * Math
-    .floor(Math.random() * (maxValue - 1)) + 1);
-  const filteredArray = randomArray.filter((card) => !excludeCards.includes(card));
-  const slicedArray = filteredArray.slice((filteredArray.length - howManyHeroes));
-  return slicedArray;
+  const invalidCards = [
+    9, 16, 19, 21, 22, 27,
+    33, 46, 47, 50, 51, 54,
+    55, 59, 62, 64, 65, 67,
+    74, 77, 85, 86, 89, 90,
+    91, 94,
+  ];
+  // list valid cards
+  const validCards = [];
+  for (let i = 1; i <= maxValue; i += 1) {
+    if (!invalidCards.includes(i)) {
+      validCards.push(i);
+    }
+  }
+  // shuffle valid cards
+  validCards.sort(() => Math.random() - 0.5);
+  // slice first cards
+  return validCards.slice(0, howManyHeroes);
 };
 
 export default draw;
