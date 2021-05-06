54fafe79e5b93e0dc35f741756c26ce6824c9ff8
Timothee <augustin.timothee@gmail.com>
2021-05-06 14:34:01 +0200

    update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -34,14 +34,17 @@ function Home() {
             La première ligne de jeu est appelée la zone de combat. C’est à cet endroit que les super-héros s’affrontent. Chaque joueur doit placer un super-héros sur la zone de combat : le super-héros actif. Un joueur ne peut avoir qu’un seul super-héros actif à la fois.
             <br />
             <br />
-            <p className="pBold">Comment gagner :</p>
-            Le premier joueur dont les points de vie (PV) arrivent à 0 perd la partie. Chaque joueur dispose de 300 points de vie au commencement de la partie. Les points de vie sont calculés et déduits à chaque fois qu’un joueur perd une carte.
-            <br />
-            <br />
             <p className="pBold">Comment l’affrontement des super-héros actifs se passe-t-il ?</p>
+            Chaque joueur dispose de 300 points de vie au commencement de la partie. Les points de vie sont calculés et déduits à chaque fois qu’un joueur perd une carte.
+            <br />
             Le combat prend en compte les statistiques suivantes de chaque carte des super-héros actifs : la force (strength), la puissance (power) et la résistance (durability).
-            Les dommages infligés à une carte sont directement égaux à la force de la carte qui l’attaque. Les dommages infligés sont soustraits à la résistance de la carte qui les subit. Lorsque la résistance d’une carte arrive à 0, le super-héros est battu, la carte est perdue.
+            <br />
+            Les dommages infligés à une carte sont directement égaux à la force de la carte qui l’attaque. Les dommages infligés sont soustraits à la résistance de la carte qui les subit.
+            <br />
+            Lorsque la résistance d’une carte arrive à 0, le super-héros est battu, la carte est perdue.
             Quand un joueur perd une carte, il perd des PV équivalents à la statistique puissance de la carte perdue.
+            <br />
+            Le premier joueur dont les points de vie (PV) arrivent à 0 perd la partie.
             <br />
             <br />
             <p className="pBold">Préparation de la partie :</p>
@@ -51,10 +54,17 @@ function Home() {
             <p className="pBold">Les étapes d’un tour :</p>
             A chaque tour, le joueur peut soit :
             Poser une carte sur la zone de combat (si il n’y en a pas).
-            Attaquer la carte de son adversaire qui se trouve dans la zone de combat.
-            Une carte ne peut pas attaquer durant le tour où elle est posée.
-            Piocher une carte (si sa main est inférieure à 5).
+            Piocher une carte (si sa main est inférieure à 5), attaquer ou utiliser une action spéciale.
+            Un joueur ne peut pas utiliser d’attaque ou d’action spéciale durant son 1er tour de jeu.
             Votre tour est terminé. C’est au tour de l’adversaire. Son tour comporte les mêmes étapes que le vôtre.
+            <br />
+            <br />
+            <p className="pBold">Les différentes actions :</p>
+            Attack : Une attaque basique, la carte inflige un montant de dégats égal à sa strength.
+            <br />
+            Power strike : Tentez une attaque puissante pour effectuer des dégats allant jusque 3 fois la strength de votre carte. Peut également infliger des dégats normaux ou échouer.
+            <br />
+            Sacrifice : Sacrifiez votre carte pour remplir votre main. Vous pourrez rejouer immédiatement mais vous subissez la même pénalité que si votre carte a été détruite normalement.
           </p>
         </div>
       </div>
