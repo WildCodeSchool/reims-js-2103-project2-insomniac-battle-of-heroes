58d8d00781be8f217d902a0e2eeb1a9d5d5720ea
HuguesBriqueler <hugues.briqueler@gmail.com>
2021-05-07 09:16:06 +0200

    Added credits component

--- src/components/Credits.jsx
+++ src/components/Credits.jsx
@@ -13,6 +13,40 @@ function Credits() {
         <div className="homeNavBar">
           <NavBar />
         </div>
+        <div className="creditsPage">
+          <div className="creditsTeam">
+            <section className="creditSection">
+              <h1>Concept :</h1>
+              <p>Jean-Marc Epis</p>
+              <p>Timothée Augustin</p>
+            </section>
+            <section className="creditsSection">
+              <h1>Development Team :</h1>
+              <p>Jean-Marc Epis</p>
+              <p>Timothée Augustin</p>
+              <p>Thomas Dietrich</p>
+              <p>Hugues Briqueler</p>
+            </section>
+            <section className="creditSection">
+              <h1>Graphic Assets - Design :</h1>
+              <p>Thomas Dietrich</p>
+            </section>
+          </div>
+          <div className="creditsRessources">
+            <section className="ressourcesSection">
+              <h1>Ressources :</h1>
+              <p>SuperHero API</p>
+              <p>https://www.superheroapi.com</p>
+            </section>
+            <img src="./images/react.png" id="reactLogo" alt="React Logo" />
+            <img src="./images/js.png" id="jsLogo" alt="JavaScript Logo" />
+            <img src="./images/css_html.png" id="cssLogo" alt="CSS & HHTML Logo" />
+          </div>
+          <section className="specialThanks">
+            <h1>Special Thanks to our ‘Code Wizard’</h1>
+            <p>Romain Guillemot</p>
+          </section>
+        </div>
       </div>
     </>
   );
